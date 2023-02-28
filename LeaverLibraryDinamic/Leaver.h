2bd7a9b1a49c7df55ca5a656c19d98dbfcf88954
MiBeloz <miha_bmv_@mail.ru>
2023-02-28 23:26:34 +0600

    Fix encoding

--- LeaverLibraryDinamic/Leaver.h
+++ LeaverLibraryDinamic/Leaver.h
@@ -1,4 +1,4 @@
-#pragma once
+п»ї#pragma once
 
 #ifdef LEAVERLIBRARYDINAMIC_EXPORTS
 #define LEAVERLIBRARY_API __declspec(dllexport)
@@ -12,7 +12,7 @@ namespace LeaverLibraryDinamic {
 	class Leaver {
 	public:
 		///<summary>
-		///Принимает на вход имя и возвращает прощание с именем
+		///РџСЂРёРЅРёРјР°РµС‚ РЅР° РІС…РѕРґ РёРјСЏ Рё РІРѕР·РІСЂР°С‰Р°РµС‚ РїСЂРѕС‰Р°РЅРёРµ СЃ РёРјРµРЅРµРј
 		/// <\summary>
 		LEAVERLIBRARY_API std::string leave(const std::string _name);
 	};
